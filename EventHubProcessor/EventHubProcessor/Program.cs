11f09a5c79b58b8f53eb19c35a77879877158b8b
ddonglee <ddonglee@icloud.com>
2017-01-17 14:47:18 +0900

    azure

---
+++ EventHubProcessor/EventHubProcessor/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+
+using Microsoft.ServiceBus.Messaging;
+
+using Microsoft.WindowsAzure.Storage;
+
+
+namespace EventHubProcessor
+{
+    class Program
+    {
+        public static Func<object, DateTime> InitialOffsetProvider { get; private set; }
+
+        static void Main(string[] args)
+        {
+
+            string eventhubConnectionString = @"";
+            string eventhubName = "messages/events";
+            string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}");
+
+            string _guId = Guid.NewGuid().ToString();
+
+            string eventProcessorHostName = _guId;
+            string leaseName = eventProcessorHostName = _guId;
+
+            EventProcessorHost eventProcessorHost = new EventProcessorHost(
+                eventProcessorHostName,
+                eventhubName,
+                EventHubConsumerGroup.DefaultGroupName,
+                eventhubConnectionString,
+                storageConnectionString,
+                leaseName);
+            Console.WriteLine("eventProcessorHost");
+
+            var opions = new EventProcessorOptions();
+            {
+                InitialOffsetProvider = (partitionId) => DateTime.UtcNow;
+            };
+            opions.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+            eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(opions).Wait();
+            Console.WriteLine("Receiving. Press enter key to stop worker.");
+            Console.ReadLine();
+            eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+        }
+
+        private static void Opions_ExceptionReceived(object sender, ExceptionReceivedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ EventHubProcessor/EventHubProcessor/SimpleEventProcessor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace EventHubProcessor
+{
+    class SimpleEventProcessor : IEventProcessor
+    {
+        Task IEventProcessor.CloseAsync(PartitionContext context, CloseReason reason)
+        {
+            Console.WriteLine("Processor Shutting Down. Partition '{0}', Reason: '{1}'.", context.Lease.PartitionId, reason);
+
+            return Task.FromResult<object>(null);
+
+        }
+        Task IEventProcessor.OpenAsync(PartitionContext context)
+        {
+            Console.WriteLine("SimpleEventProcessor initialized Partition: '{0}', offset: '{1}'", context.Lease.PartitionId, context.Lease.Offset);
+
+            return Task.FromResult<object>(null);
+        }
+        async Task IEventProcessor.ProcessEventsAsync(PartitionContext context, IEnumerable<EventData> messages)
+        {
+            foreach(EventData eventdata in messages)
+            {
+                string data = Encoding.UTF8.GetString(eventdata.GetBytes());
+
+                Console.WriteLine(string.Format("Message received. Partition: {0}, Data: {1} {2}", context.Lease.PartitionId, data, eventdata.EnqueuedTimeUtc));
+            }
+
+           
+        }
+
+    }
+}
